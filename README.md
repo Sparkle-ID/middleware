2a89b4ab8d314f3a633426b1468af66f8f15ca98
FibrinLab <fibrinlab@gmail.com>
2024-12-16 14:27:24 +0000

    feat: added the sparkleid common package

---
+++ README.md
@@ -0,0 +1,6 @@
+# Sparkle ID
+
+
+
+
+https://www.npmjs.com/package/@sparkleid/common
--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,8 +1,8 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
-import { validateRequest } from '../middlewares/validate-request';
+import { validateRequest } from '@sparkleid/common';
 import { User } from '../models/user';
-import { BadRequestError } from '../errors/bad-request-error';
+import { BadRequestError } from '@sparkleid/common';
 import { Password } from '../services/password';
 import jwt from 'jsonwebtoken';
 
